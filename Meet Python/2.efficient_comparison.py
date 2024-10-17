29a8bb770c7bef93016e0c7c631ac7005d3faada
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-10-17 08:23:37 -0400

    Create 2.efficient_comparison.py

---
+++ Meet Python/2.efficient_comparison.py
@@ -0,0 +1,16 @@
+# 2
+
+# You would like to write a function that takes
+# integers x, y, L and R as parameters, and returns
+# True of x^y lies within the interval (L, R] and
+# False otherwise. You're considering several
+# different ways to write the conditional statement
+# inside the function:
+# 1. if l < x ** y <= R:
+# 2. if x ** y > L and x ** y <= R:
+# 3. if x ** y in range(L + 1, R + 1)
+# Which option would be the most efficient in terms
+# of execution time?
+
+# Answer: Option 1 is the most optimal one.
+
